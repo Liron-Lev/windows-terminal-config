c175a484588c4bd774376ffba4aedb01f90f3971
Liron Lev <lironlev2604@gmail.com>
2024-06-14 14:47:53 +0300

    images

--- install.ps1
+++ install.ps1
@@ -5,5 +5,9 @@ Push-Location .
 cd "$env:LOCALAPPDATA\Programs\oh-my-posh\bin"
 oh-my-posh.exe init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
 Pop-Location
+
 New-Item "$env:HOMEPATH\Documents\PowerShell\" -ItemType Directory -ea 0
 Copy-Item ./Microsoft.PowerShell_profile.ps1 "$env:HOMEPATH\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+
+Copy-Item "settings.json" $env:LocalAppData\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json
+Copy-Item "images" $env:LocalAppData\Packages\Microsoft.WindowsTerminal_*\LocalState\
